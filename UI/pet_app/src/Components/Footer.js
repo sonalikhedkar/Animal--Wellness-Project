90bd2b15f149dc5a030b8a5665a628adbeed4507
Sonali Khedkar <khedkarsona9@gmail.com>
2024-02-12 16:41:11 +0530

    components added

--- UI/pet_app/src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Footer.css'
-export default function Footer() {
-  return (
-    <div><h4 className="footer">
-        &copy; copyrights reseved
-    </h4></div>
-  )
-}
--- UI/pet_app/src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Footer.css'
-export default function Header() {
-  return (
-    <div>
-        <h1 className="header">Pet Wellness Hub</h1>
-    </div>
-  )
-}
