8d5755a06c0ed6017378c7745ba415ce9f36df5c
sonalikhedkar <110111616+sonalikhedkar@users.noreply.github.com>
2024-02-26 16:47:08 +0530

    Update backend

--- Backend/src/main/java/com/app/EComBackend13Application.java
+++ Backend/src/main/java/com/app/EComBackend13Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+//all components from app package 
 @ComponentScan({ "com.app.*" })
 @SpringBootApplication
 public class EComBackend13Application {
